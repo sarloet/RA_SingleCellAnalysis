c889cbeb9f9434e599b93d823003efe10cf0b894
sarloet <sarahloetscher@hotmail.ch>
2024-04-11 16:50:30 +0200

    fix

---
+++ code/standard_libraries.R
@@ -0,0 +1,23 @@
+#load Packages
+suppressPackageStartupMessages({
+  library(BiocParallel)
+  library(ggplot2)
+  library(dplyr)
+  library(scater)
+  library(scran)
+  library(DropletUtils)
+  library(scDblFinder)
+  library(batchelor)
+  library(reticulate)
+  library(bluster)
+  library(sctransform)
+  library(scuttle)
+  library(dittoSeq)
+  library(pheatmap)
+  library(xlsx)
+  library(GSEABase)
+  library(gridExtra)
+  library(presto)
+  #library(BiocSingular) 02
+})
+
